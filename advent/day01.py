ca205cdedb8d32f2280b8fe92e1327e671fe5bee
Cora Lenz <a11933628@unet.univie.ac.at>
2024-12-03 10:11:49 +0100

    finished harder advent

--- advent/day01.py
+++ advent/day01.py
@@ -3,6 +3,13 @@
 # chief historian's office; comparing reports
 from util import read_input
 
+def count_occurrences(x, l):
+    #function that counts occurances of number x in list l
+    counter=0
+    for number in l:
+        if x==number:
+            counter+=1
+    return counter
 # first, read the raw data
 raw = read_input("../advent_data/day01.txt")
 list_a = []
@@ -28,4 +35,9 @@ for a, b in zip(list_a, list_b):
     diff = abs(a - b)
     sum_of_differences += diff
 
-print(sum_of_differences)
+#print(sum_of_differences)
+result=0
+for number in list_a:
+    result+=number*count_occurrences(number, list_b)
+
+print(result)
